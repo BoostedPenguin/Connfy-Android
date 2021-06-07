96f60dd0a8c226447718e70bdcd75c2b80f4c080
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2021-06-07 16:19:42 +0200

    added api key and initialized gmap

--- app/src/main/java/com/industryproject/connfy/ui/meeting_view/MeetingViewFragment.kt
+++ app/src/main/java/com/industryproject/connfy/ui/meeting_view/MeetingViewFragment.kt
@@ -8,9 +8,29 @@ import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.industryproject.connfy.R
 
 class MeetingViewFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val eindhoven = LatLng(51.441642, 5.4697225)
+        googleMap.addMarker(MarkerOptions().position(eindhoven).title("Marker in Eindhoven"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eindhoven, 14f))
+    }
 
     companion object {
         fun newInstance() = MeetingViewFragment()
@@ -26,6 +46,11 @@ class MeetingViewFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        //google map initialization
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+
         viewModel = ViewModelProvider(this).get(MeetingViewViewModel::class.java)
         // TODO: Use the ViewModel
     }
